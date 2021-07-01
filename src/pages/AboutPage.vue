111f4ca8ccfd921016acf48f7c9ff5c720fc1297
Sherif Abushadi <sherif@amgando.org>
2021-07-01 11:35:50 +0300

    add content to About page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,8 +2,7 @@
   <div class="sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
     <div class="mx-auto max-w-7xl px-8">
       <h1 class="my-14 text-gray-400 tracking-tight font-extrabold text-4xl lg:mt-6">About this Project</h1>
-      <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
-      <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+      <div class="lg:mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
         <div>
           <h2 class="text-base text-near-royal font-semibold tracking-wide uppercase">the beginning</h2>
           <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Meet Winston Robson</h3>
@@ -21,29 +20,21 @@
           </svg>
           <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
             <figure>
-              <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
-                <img class="rounded-lg shadow-lg object-cover object-center" src="@/assets/WinstonRobson-front.png" width="1184" height="1376" />
+              <div class="lg:ml-24">
+                <img class="rounded-lg shadow-lg object-cover object-center" src="@/assets/WinstonRobson-front.png" width="250" />
               </div>
             </figure>
           </div>
         </div>
         <div class="mt-8 lg:mt-0">
-          <div class="mt-5 space-y-2 prose text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
-            <p>Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.</p>
-            <p>Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.</p>
-            <p>Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.</p>
-            <ul class="list-disc list-inside">
-              <li>Quis elit egestas venenatis mattis dignissim.</li>
-              <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-            </ul>
-            <p>Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.</p>
-            <p>Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa, lectus. Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.</p>
-            <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
+          <div class="space-y-2 prose text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+            <p>Winston is a business- minded data scientist with a love for truth and understandable answers.</p>
+            <p>His work experience ranges from implementing bleeding edge data lake solutions to the development and instruction of intro to Excel curriculum.</p>
+            <p>Airbnb hosts have described Winston as "amazing as a guest could be! He is friendly, respectful, appreciative, open minded and clean! I would have him back at any time!"</p>
           </div>
         </div>
       </div>
-      <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none mt-12">
+      <div class="lg:mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none mt-12">
         <div>
           <h2 class="text-base text-near-royal font-semibold tracking-wide uppercase">the middle</h2>
           <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Enter NEAR.science</h3>
@@ -51,13 +42,13 @@
       </div>
       <div class="mt-8 lg:mt-0">
         <div class="mt-5 space-y-2 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
-          <p>Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.</p>
-          <p>Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.</p>
-          <p>Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa, lectus. Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.</p>
-          <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
+          <p>This website is an experiment in data science as a service.</p>
+          <p>The question we are trying to answer is whether people in the community are interested in asking Winston to help them make sense of on-chain data.</p>
+          <p>Take a look at the <router-link to="/requests" class="text-near-royal">Requests</router-link> page to upvote existing requests to help Winston prioritize his time.</p>
+          <p>You can also submit a new request that might capture his imagination as well as the upvotes of other readers on this site.</p>
         </div>
       </div>
-      <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none mt-12">
+      <div class="lg:mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none mt-12">
         <div>
           <h2 class="text-base text-near-royal font-semibold tracking-wide uppercase">the end?</h2>
           <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">What's your science?</h3>
@@ -66,9 +57,11 @@
       <div class="mt-8 lg:mt-0">
         <div class="mt-5 space-y-2 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
           <p>Are you interested in contributing to <strong>NEAR.science</strong>?</p>
-          <button @click="openForm" type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-near-royal hover:bg-near-royal-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-near-royal-light uppercase">register here</button>
+          <p>If you are, please register by clicking the button below and sharing your interests.</p>
+          <p>We're always looking for new contributors. Besides, Winston would like some company.</p>
         </div>
       </div>
+      <button @click="openForm" type="button" class="my-12 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-near-royal hover:bg-near-royal-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-near-royal-light uppercase">register as a contributor to NEAR.science</button>
     </div>
   </div>
 </template>
